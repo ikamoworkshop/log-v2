5655999b48e491a7fe1de2302329abf422c82e2f
Jeremy Chang <changjeremy0226@gmail.com>
2024-08-02 08:07:41 -0700

    Updated face model

--- src/experience/sources.js
+++ src/experience/sources.js
@@ -3,5 +3,25 @@ export default [
         name: 'defaultFace',
         type: 'gltfModel',
         path: '/model/default.glb'
+    },
+    {
+        name: 'travelFace',
+        type: 'gltfModel',
+        path: '/model/travel.glb'
+    },
+    {
+        name: 'designFace',
+        type: 'gltfModel',
+        path: '/model/design.glb'
+    },
+    {
+        name: 'milestoneFace',
+        type: 'gltfModel',
+        path: '/model/milestone.glb'
+    },
+    {
+        name: 'workspaceFace',
+        type: 'gltfModel',
+        path: '/model/workspace.glb'
     }
 ]
