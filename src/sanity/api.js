4aadc95c5b3ff483a431489a22d97dc053fb8325
Jeremy Chang <changjeremy0226@gmail.com>
2024-08-08 20:15:56 -0700

    Update CROS setting and minor API updates for more flexible development.

--- src/sanity/api.js
+++ src/sanity/api.js
@@ -1,8 +1,16 @@
-import { sanityClient  } from "sanity:client"
+import {createClient} from '@sanity/client'
+
+const client = createClient({
+    projectId: 'z49s437i',
+    dataset: 'production',
+    useCdn: true,
+    // token: 'your_sanity_token_here',  // replace with your own token
+    apiVersion: '2024-08-08'
+})
 
 export async function getAllGallery(){
 
-    const gallery = await sanityClient.fetch(`*[_type == "gallery" && defined(slug)]`)
+    const gallery = await client.fetch(`*[_type == "gallery"]`)
     .catch(error => console.log(error))
 
     return gallery
