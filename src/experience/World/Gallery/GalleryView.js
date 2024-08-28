dd9efa1da2fa4dde378f0084f380569794280d42
Jeremy Chang <changjeremy0226@gmail.com>
2024-08-28 02:28:01 -0700

    Update scroll progress component to work on page reload.

--- src/experience/World/Gallery/GalleryView.js
+++ src/experience/World/Gallery/GalleryView.js
@@ -34,7 +34,7 @@ export default class GalleryView {
     setCamera(){
         this.camera = new THREE.PerspectiveCamera(45, this.sizes.width / this.sizes.height, 0.1, 100)
         this.camera.position.set(0, 0, 5)
-        // this.scene.add(this.camera)
+        this.scene.add(this.camera)
     }
 
     getImage(){
